04532e233513e453e78f5a19f799bfe1ac351f9b
MININT-O0TSVII\Administrator <gamevagrant@gmail.com>
2018-04-18 20:43:52 +0800

    提交丢失文件

--- Assets/Scripts/qy/Data/PlayerModel.cs
+++ Assets/Scripts/qy/Data/PlayerModel.cs
@@ -46,37 +46,111 @@ namespace qy
 
         public int BuyHeart()
         {
-            throw new System.NotImplementedException();
+            GameMainManager.Instance.netManager.BuyHeart((ret, res) => { });
+            int cost = GameMainManager.Instance.configManager.settingConfig.livesPrice;
+            if(playerData.coinNum<cost)
+            {
+                Debug.LogError("金币不足！");
+                return (int)ErrType.NOT_ENOUGH_COIN;
+            }
+
+            playerData.coinNum -= cost;
+            playerData.heartNum = GameMainManager.Instance.configManager.settingConfig.maxLives;
+            playerData.recoveryLeftTime = 0;
+            SaveData();
+
+            return (int)ErrType.NULL;
         }
 
         public int BuyProp(string itemId, int num)
         {
-            throw new System.NotImplementedException();
+            GameMainManager.Instance.netManager.BuyItem(itemId,num,(ret, res) => { });
+            PropItem item = GameMainManager.Instance.configManager.propsConfig.GetItem(itemId);
+            if(item==null)
+            {
+                Debug.Log("物品不存在");
+                return (int)ErrType.PROP_ID_ERROR;
+            }
+            int cost = item.price * num;
+            if(playerData.coinNum<cost)
+            {
+                Debug.LogError("金币不足！");
+                return (int)ErrType.NOT_ENOUGH_COIN;
+            }
+
+            playerData.coinNum -= cost;
+            playerData.AddPropItem(itemId,num);
+            SaveData();
+
+            return (int)ErrType.NULL;
         }
 
         public int EndLevel(int level, bool result, int step, int wingold)
         {
-            throw new System.NotImplementedException();
+            GameMainManager.Instance.netManager.LevelEnd(level,result?1:0,step, wingold, (ret, res) => { });
+            if(result)
+            {
+                MatchLevelItem matchItem = GameMainManager.Instance.configManager.matchLevelConfig.GetItem((1000000+level).ToString());
+                playerData.coinNum += wingold + matchItem.coin;
+                playerData.starNum += matchItem.star;
+                playerData.heartNum += 1;
+                playerData.eliminateLevel += 1;
+
+                foreach(PropItem prop in matchItem.itemReward)
+                {
+                    playerData.AddPropItem(prop.id,prop.count);
+                }
+                SaveData();
+            }
+
+            return (int)ErrType.NULL;
         }
 
         public int ModifyNickName(string nickName)
         {
-            throw new System.NotImplementedException();
+            GameMainManager.Instance.netManager.ModifyNickName(nickName, (ret, res) => { });
+            playerData.nickName = nickName;
+            SaveData();
+            return (int)ErrType.NULL;
         }
 
         public int QuestComplate(string questId)
         {
-            throw new System.NotImplementedException();
+            GameMainManager.Instance.netManager.UpdateQuestId(questId,(ret,res)=> { });
+            config.QuestItem quest = GameMainManager.Instance.configManager.questConfig.GetItem(questId);
+            if(quest==null)
+            {
+                return (int)ErrType.QUEST_ID_ERROR;
+            }
+            playerData.questId = questId;
+            SaveData();
+            return (int)ErrType.NULL;
         }
 
         public int StartLevel()
         {
-            throw new System.NotImplementedException();
+            GameMainManager.Instance.netManager.LevelStart((ret, res) => { });
+            if(playerData.heartNum<=0)
+            {
+                return (int)ErrType.NOT_ENOUGH_HEART;
+            }
+            playerData.heartNum -= 1;
+            SaveData();
+
+            return (int)ErrType.NULL;
         }
 
         public int UseProp(string itemID, int count)
         {
-            throw new System.NotImplementedException();
+            PropItem prop = playerData.GetPropItem(itemID);
+            if(prop==null || prop.count<count)
+            {
+                return (int)ErrType.NOT_ENOUGH_PROP;
+            }
+
+            playerData.RemovePropItem(itemID, count);
+
+            return (int)ErrType.NULL;
         }
 
         private void SaveData()
