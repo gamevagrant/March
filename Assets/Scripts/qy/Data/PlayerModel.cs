6ae7240449df5c9d46cd741af5608f3b0fcc0125
MININT-O0TSVII\Administrator <gamevagrant@gmail.com>
2018-04-18 19:02:04 +0800

    修复文件丢失的问题

---
+++ Assets/Scripts/qy/Data/PlayerModel.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using qy.config;
+
+namespace qy
+{
+    public class PlayerModel : IPlayerModel
+    {
+        private PlayerData playerData;
+        public PlayerModel(PlayerData playerData)
+        {
+            this.playerData = playerData;
+        }
+        public enum ErrType:int
+        {
+            NULL=0,
+            NOT_ENOUGH_COIN,
+            NOT_ENOUGH_HEART,
+            NOT_ENOUGH_PROP,
+            QUEST_ID_ERROR,
+            PROP_ID_ERROR,
+        }
+
+        public int BuyFiveMore(int step)
+        {
+            GameMainManager.Instance.netManager.EliminateLevelFiveMore(step, (ret, res) => { });
+
+            int cost = GameMainManager.Instance.configManager.settingConfig.GetPriceWithStep(step);
+            List<PropItem> props = GameMainManager.Instance.configManager.settingConfig.GetBonusItemBagWithStep(step); 
+            if(playerData.coinNum<cost)
+            {
+                Debug.LogError("金币不足！");
+                return (int)ErrType.NOT_ENOUGH_COIN;
+            }
+
+            playerData.coinNum -= cost;
+            foreach(PropItem prop in props)
+            {
+                playerData.AddPropItem(prop.id,prop.count);
+            }
+            SaveData();
+
+            return (int)ErrType.NULL;
+        }
+
+        public int BuyHeart()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int BuyProp(string itemId, int num)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int EndLevel(int level, bool result, int step, int wingold)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int ModifyNickName(string nickName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int QuestComplate(string questId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int StartLevel()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int UseProp(string itemID, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SaveData()
+        {
+            LocalDatasManager.playerData = playerData;
+        }
+    }
+}
+
---
+++ Assets/Scripts/qy/Interface/IPlayerModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace qy
+{
+    public interface IPlayerModel
+    {
+        /// <summary>
+        /// 完成任务
+        /// </summary>
+        /// <param name="questId">下一个任务</param>
+        /// <returns></returns>
+        int QuestComplate(string questId);
+        /// <summary>
+        /// 使用道具
+        /// </summary>
+        /// <param name="itemID"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        int UseProp(string itemID, int count);
+        /// <summary>
+        /// 购买道具
+        /// </summary>
+        /// <param name="itemId"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        int BuyProp(string itemId, int num);
+        /// <summary>
+        /// 开始任务
+        /// </summary>
+        /// <returns></returns>
+        int StartLevel();
+        /// <summary>
+        /// 结束任务
+        /// </summary>
+        /// <param name="level">关卡数</param>
+        /// <param name="result">是否胜利</param>
+        /// <param name="step">使用步数</param>
+        /// <param name="wingold">非固定奖励</param>
+        /// <returns></returns>
+        int EndLevel(int level, bool result, int step, int wingold);
+        /// <summary>
+        /// 购买生命
+        /// </summary>
+        /// <returns></returns>
+        int BuyHeart();
+        /// <summary>
+        /// 购买 再来五步
+        /// </summary>
+        /// <param name="step"></param>
+        /// <returns></returns>
+        int BuyFiveMore(int step);
+        /// <summary>
+        /// 更改昵称
+        /// </summary>
+        /// <param name="nickName"></param>
+        /// <returns></returns>
+        int ModifyNickName(string nickName);
+
+
+    }
+}
+
