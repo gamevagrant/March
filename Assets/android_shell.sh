946bf85095d15d4c5d686dd665d3bfbc2b182833
CuiXiaoliang <stonebyme@gmail.com>
2018-04-21 20:39:59 +0800

    Ready for CI.

---
+++ Assets/android_shell.sh
@@ -0,0 +1,35 @@
+﻿
+echo "Build Start"
+
+# Android Stuff.
+export ANDROID_KEYSTORE_NAME=user.keystore
+export ANDROID_KEYSTORE_PASSWORD=111111
+export ANDROID_KEYALIAS_NAME=test
+export ANDROID_KEYALIAS_PASSWORD=111111
+export ANDROID_SDK_ROOT=
+
+# Build folder structure.
+export APK_OUTPUT_DIR=package
+
+# Unity app path.
+export untiy=/Applications/Unity_2017.2.0f3/Unity.app/Contents/MacOS/Unity
+
+# Project path.
+export projectPath=.
+
+# Fetch argments defined in Jenkins.
+echo "version = $Version"
+echo "IsForDev = $IsForDev"
+
+# Check apk output dir.
+if ["$APK_OUTPUT_DIR" == ""]
+then
+	echo $APK_OUTPUT_DIR is undefined.
+	exit 0
+else
+	echo output dir is $APK_OUTPUT_DIR.
+fi
+
+# Unity batch mode build.
+echo "Compiling.. this will take a while"
+$untiy -quit -batchmode -projectPath $projectPath -logFile `pwd`/editor.log -executeMethod ProjectBuild.JenkinsBuildAndroid
