1f3d1e176211b668299c700cbc1806a9097795f8
CuiXiaoliang <stonebyme@gmail.com>
2018-04-30 16:20:57 +0800

    Client could change device id.

--- Assets/Scripts/common/Utils.cs
+++ Assets/Scripts/common/Utils.cs
@@ -1,29 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Assets.Scripts.Common;
-using UnityEngine;
+﻿using Assets.Scripts.Common;
 using System.Text.RegularExpressions;
+using UnityEngine;
 
 public class Utils : MonoSingleton<Utils>
 {
+    public string DeviceId = SystemInfo.deviceUniqueIdentifier;
 
-    public string A = "^[0-9]+$";//纯数字检测
-    public string B = "^[A-Za-z0-9]+$"; //.数字或英文
-    public string C = "^[\u4e00-\u9fa5]+$"; //纯汉字
-     
+    public const string A = "^[0-9]+$";//纯数字检测
+    public const string B = "^[A-Za-z0-9]+$"; //.数字或英文
+    public const string C = "^[\u4e00-\u9fa5]+$"; //纯汉字
+
     /// <summary>
     /// 获取当前设备id
     /// </summary>
     /// <returns></returns>
     public string getDeviceID()
     {
-        string deviceId = SystemInfo.deviceUniqueIdentifier;
-        return deviceId;
+        return DeviceId;
     }
 
     public bool isValid(string _content)
     {
-        Regex reg =  new Regex(C);
+        Regex reg = new Regex(C);
         return reg.IsMatch(_content);
     }
 
@@ -42,7 +40,7 @@ public class Utils : MonoSingleton<Utils>
     public bool isStrLengthValid(string _content)
     {
         int length = _content.Length;
-        if (3 <= length && length<= 16)
+        if (3 <= length && length <= 16)
             return true;
         else
             return false;
