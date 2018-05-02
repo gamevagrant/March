133d81971a92397f09b9d2d26aeebe2e722db7ac
CuiXiaoliang <stonebyme@gmail.com>
2018-05-02 21:06:10 +0800

    test

--- test_shell.sh
+++ test_shell.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
 # Fetch argments defined in Jenkins.
-echo "version = $Version"
-echo "IsForDev = $IsForDev"
-
-echo "$gitlabBranch"
-echo "$gitlabSourceBranch"
 echo $gitlabActionType
 echo $gitlabUserName
-echo $gitlabUserEmail
 echo $GIT_COMMIT
-
-echo "Last commit=$LAST_COMMIT"
 
 export COMMIT_MESSAGE=\"$(git log --format=oneline -n 1 $CIRCLE_SHA1)\"
 echo $COMMIT_MESSAGE
 
-export LAST_COMMIT=$GIT_COMMIT
-echo "Last commit=$LAST_COMMIT"
-
-echo $JENKINS_HOME
-echo $WORKSPACE
 echo $CHANGES_SINCE_LAST_BUILD
+
+file="./env.txt"
+
+if [ -f "$file" ]
+then
+	echo "$file found."
+    
+    COMMIT_MESSAGE=$(cat "$file")
+    echo $COMMIT_MESSAGE
+    
+    cat $COMMIT_MESSAGE
+
+else
+	echo "$file not found."
+    
+    touch $file
+	echo $COMMIT_MESSAGE>>$file
+fi
