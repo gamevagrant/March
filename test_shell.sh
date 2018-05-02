8a04f2a07b89a62fd27c8169d58be975cbbfc41c
CuiXiaoliang <stonebyme@gmail.com>
2018-05-02 16:21:00 +0800

    test

--- test_shell.sh
+++ test_shell.sh
@@ -16,5 +16,5 @@ echo "Last commit=$LAST_COMMIT"
 export COMMIT_MESSAGE=\"$(git log --format=oneline -n 1 $CIRCLE_SHA1)\"
 echo $COMMIT_MESSAGE
 
-export LAST_COMMIT=$GIT_COMMIT
+$LAST_COMMIT=$GIT_COMMIT
 echo "Last commit=$LAST_COMMIT"
