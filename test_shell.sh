9e9e3bba71031acf1d74a95b9e7fbd6ad38efad2
CuiXiaoliang <stonebyme@gmail.com>
2018-05-02 14:42:26 +0800

    adding test shell.

---
+++ test_shell.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Fetch argments defined in Jenkins.
+echo "version = $Version"
+echo "IsForDev = $IsForDev"
+
+echo $gitlabBranch
+echo $gitlabSourceBranch
+echo $gitlabActionType
+echo $gitlabUserName
+echo $gitlabUserEmail
+echo $gitlabSourceRepoHomepage
+echo $gitlabSourceRepoName
+echo $gitlabSourceNamespace
+echo $gitlabSourceRepoURL
+echo $gitlabSourceRepoSshUrl
+echo $gitlabSourceRepoHttpUrl
+echo $gitlabMergeRequestTitle
+echo $gitlabMergeRequestDescription
+echo $gitlabMergeRequestId
+echo $gitlabMergeRequestIid
+echo $gitlabMergeRequestState
+echo $gitlabMergedByUser
+echo $gitlabMergeRequestAssignee
+echo $gitlabMergeRequestLastCommit
+echo $gitlabMergeRequestTargetProjectId
+echo $gitlabTargetBranch
+echo $gitlabTargetRepoName
+echo $gitlabTargetNamespace
+echo $gitlabTargetRepoSshUrl
+echo $gitlabTargetRepoHttpUrl
+echo $gitlabBefore
+echo $gitlabAfter
+echo $gitlabTriggerPhrase
