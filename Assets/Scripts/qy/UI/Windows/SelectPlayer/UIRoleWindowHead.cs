eeda02f7bd4f5bb000d2b1aaf24c163b40471ce4
CuiXiaoliang <stonebyme@gmail.com>
2018-05-08 22:27:11 +0800

    Fix story person asset bundle update.

--- Assets/Scripts/qy/UI/Windows/SelectPlayer/UIRoleWindowHead.cs
+++ Assets/Scripts/qy/UI/Windows/SelectPlayer/UIRoleWindowHead.cs
@@ -22,12 +22,18 @@ public class UIRoleWindowHead : BaseItemView {
     {
         this.data = data as RoleItem;
 
-        string headUrl = FilePathTools.GetPersonHeadPath(this.data.headIcon);
-        AssetsManager.Instance.LoadAssetAsync<Sprite>(headUrl, (sp) =>
-        {
-            head.sprite = sp;
-            GameUtils.Scaling(head.transform as RectTransform, new Vector2(sp.texture.width, sp.texture.height));
-        });
+        var sp =
+            March.Core.ResourceManager.ResourceManager.instance.Load<Sprite>(Configure.StoryPerson,
+                this.data.headIcon);
+        head.sprite = sp;
+        GameUtils.Scaling(head.transform as RectTransform, new Vector2(sp.texture.width, sp.texture.height));
+
+        //string headUrl = FilePathTools.GetPersonHeadPath(this.data.headIcon);
+        //AssetsManager.Instance.LoadAssetAsync<Sprite>(headUrl, (sp) =>
+        //{
+        //    head.sprite = sp;
+        //    GameUtils.Scaling(head.transform as RectTransform, new Vector2(sp.texture.width, sp.texture.height));
+        //});
 
         qy.PlayerData.RoleState state = qy.GameMainManager.Instance.playerData.GetRoleState(this.data.id);
         switch (state)
