d488e50787fd98eff5d261e8c1951892dcb8f009
CuiXiaoliang <stonebyme@gmail.com>
2018-05-09 21:49:02 +0800

    Loading simulation.

---
+++ Assets/March/Scripts/Loading/LoadingProgressController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class LoadingProgressController : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float Min;
+
+    [Range(0f, 1f)]
+    public float PreMax;
+
+    private Image bar;
+    private Image barRight;
+    private Text percentageText;
+
+    public float PreMaxDuration;
+    public float MaxDuration;
+
+    private Tweener tweener;
+    private Tweener tweener2;
+
+    private void Awake()
+    {
+        bar = transform.Find("bg/bar").GetComponent<Image>();
+        barRight = transform.Find("bg/bar_right").GetComponent<Image>();
+        percentageText = transform.Find("bg/right").GetComponent<Text>();
+
+        bar.fillAmount = Min;
+        NotifyUI();
+    }
+
+    private void Start()
+    {
+        tweener = bar.DOFillAmount(PreMax, PreMaxDuration).OnUpdate(NotifyUI).OnComplete(CompleteTween);
+        Debug.LogWarning("Tween 1 start");
+    }
+
+    void CompleteTween()
+    {
+        Debug.LogWarning("Tween 1 complete " + bar.fillAmount);
+
+        Debug.LogWarning("Tween 2 start " + bar.fillAmount);
+
+        tweener2 = bar.DOFillAmount(1f, PreMaxDuration).OnUpdate(NotifyUI).OnComplete(TrimTween2);
+    }
+
+    void CompleteTween2()
+    {
+        Debug.LogWarning("Tween 2 complete " + bar.fillAmount);
+    }
+
+    public void TrimTween()
+    {
+        tweener.Complete();
+    }
+
+    public void TrimTween2()
+    {
+        tweener2.Complete();
+    }
+
+    private void NotifyUI()
+    {
+        percentageText.text = string.Format("{0:0}%", bar.fillAmount * 100);
+
+        if (bar.fillAmount >= 1f)
+        {
+            barRight.gameObject.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/UI/LoadingScene.cs
+++ Assets/Scripts/UI/LoadingScene.cs
@@ -8,14 +8,14 @@ using UnityEngine.UI;
 public class LoadingScene : MonoBehaviour
 {
     private LoadingSceneLoader loader;
+    private LoadingProgressController progressController;
 
-    private AsyncOperation m_async;
-    private Image m_progressBar;
-    private Image m_progressBar_right;
+    private AsyncOperation asyncOperation;
 
     private void Awake()
     {
         loader = GetComponent<LoadingSceneLoader>();
+        progressController = GameObject.Find("Canvas/ProgressBar").GetComponent<LoadingProgressController>();
 
         Messenger.AddListener<string>(ELocalMsgID.LoadScene, OnLoadSceneHandle);
 
@@ -27,13 +27,7 @@ public class LoadingScene : MonoBehaviour
 
     IEnumerator Start()
     {
-        Debug.Log("LoadingScene Start ...");
-
-        var canvas = FindObjectOfType<Canvas>().gameObject;
-        m_progressBar = canvas.transform.Find("ProgressBar").GetComponent<Image>();
-        var go = canvas.transform.Find("ProgressBar_bg");
-        m_progressBar_right = go.transform.Find("right").GetComponent<Image>();
-       
+        Debug.Log("LoadingScene Start ...");       
 
 #if GAME_DEBUG
         Instantiate(Resources.Load<GameObject>(Configure.ReporterPath));
@@ -41,23 +35,14 @@ public class LoadingScene : MonoBehaviour
 
         yield return loader.Load();
 
+        progressController.TrimTween();
+
         GameMainManager.Instance.configManager.LoadConfig(() =>
         {
+            progressController.TrimTween2();
+
             StartCoroutine(LoadScene("Film"));
         });
-    }
-
-    void Update()
-    {
-        if (m_async != null && m_progressBar != null)
-        {
-            m_progressBar.fillAmount = m_async.progress;
-            Debug.Log("------" + m_async.progress);
-            if (m_async.progress >= 1.0f)
-            {
-                m_progressBar_right.gameObject.SetActive(true);
-            }
-        }
     }
 
     private void AddConfig<T>() where T : DatabaseConfig, new()
@@ -69,8 +54,8 @@ public class LoadingScene : MonoBehaviour
     private IEnumerator LoadScene(string scene)
     {
         //m_async = SceneManager.LoadSceneAsync("main");
-        m_async = SceneManager.LoadSceneAsync(scene);
-        yield return m_async;
+        asyncOperation = SceneManager.LoadSceneAsync(scene);
+        yield return asyncOperation;
         Login();
     }
 
