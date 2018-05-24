430d101d63bfa88a7843a3a289bf9102402b02dd
CuiXiaoliang <stonebyme@gmail.com>
2018-05-24 18:59:49 +0800

    Custom controller for guide level 9_2.

---
+++ Assets/March/Scripts/Guide/CustomController/Guide_9_2Controller.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Guide_9_2Controller : MonoBehaviour
+{
+    private const string ShavolPrefab = "Shavol";
+    private Animator shavol;
+
+    void Start()
+    {
+        shavol = Instantiate(Resources.Load<GameObject>(ShavolPrefab), transform).GetComponent<Animator>();
+    }
+}
